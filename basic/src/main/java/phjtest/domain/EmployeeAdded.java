66918d8246b94eecb2c687ddf6457ecfd0383a39
phj9719 <61671576+phj9719@users.noreply.github.com>
2023-05-16 09:43:32 +0900

    commit

---
+++ basic/src/main/java/phjtest/domain/EmployeeAdded.java
@@ -0,0 +1,25 @@
+package phjtest.domain;
+
+import java.util.*;
+import lombok.*;
+import phjtest.domain.*;
+import phjtest.infra.AbstractEvent;
+
+@Data
+@ToString
+public class EmployeeAdded extends AbstractEvent {
+
+    private Long id;
+    private String userId;
+    private RankId rankId;
+    private DepartmentId departmentId;
+    private List<EmployeeUser> employeeUser;
+
+    public EmployeeAdded(Employee aggregate) {
+        super(aggregate);
+    }
+
+    public EmployeeAdded() {
+        super();
+    }
+}
